8fcf6d998f75a5998ced684b87e3028403e09c5a
Jaydeep <jaydeepmukherjee2003@gmail.com>
2026-06-04 14:10:10 +0530

    changed tool context

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from services.chat_service import ask_llm
-#from services.weather_service import get_weather
 from tools.weather_tool import get_weather
 from memory.chat_memory import messages
 from router.intent_router import is_weather_query
@@ -11,10 +10,11 @@ while True:
     user_input = input("You: ")
 
     if user_input.lower() == "exit":
+        print("Ending chat...")
         break
 
+    # WEATHER QUERY
     if is_weather_query(user_input):
-        #print("[Router] weather tool wokring") just to see the debug
 
         weather_info = get_weather()
 
@@ -24,24 +24,47 @@ User Question:
 
 Weather Data:
 {weather_info}
+
+Answer the user's complete question.
+Use the weather data if relevant.
+If the user asks multiple things, answer all of them.
 """
 
-        messages.append({
+        # Temporary conversation (tool context only)
+        temp_messages = messages.copy()
+
+        temp_messages.append({
             "role": "user",
             "content": enhanced_prompt
         })
 
-    else:
-        #print("[router] normal chat")
+        ai_reply = ask_llm(temp_messages)
 
+        print(f"\nAI: {ai_reply}\n")
+
+        # Store only the original conversation
         messages.append({
             "role": "user",
             "content": user_input
         })
 
+        messages.append({
+            "role": "assistant",
+            "content": ai_reply
+        })
+
+        continue
+
+    # NORMAL CHAT
+    messages.append({
+        "role": "user",
+        "content": user_input
+    })
+
     ai_reply = ask_llm(messages)
 
     print(f"\nAI: {ai_reply}\n")
+
     messages.append({
         "role": "assistant",
         "content": ai_reply
